219f38a22df22ba9d988d5f157962f6e49b5f4dc
Alexander Vielma <vielmaa1804@gmail.com>
2024-09-16 14:58:14 -0400

    Finished hide-unhide navbar fuctionality

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const ARROW = document.querySelector(".hide-unhide-arrow-btn");
+const BODY = document.querySelector("body");
 
 ARROW.addEventListener("click", (e) => {
     let arrowBtn = e.target;
     arrowBtn.classList.toggle("hide-unhide-arrow-btn");
     arrowBtn.classList.toggle("hide-unhide-arrow-btn-pright");
-    
+    BODY.classList.toggle("body");
+    console.log(BODY);
 });
